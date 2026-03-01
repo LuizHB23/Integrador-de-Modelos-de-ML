8b1d71ea1c2b673e0dde91127456d2764961e8ed
LuizHB23 <lhb191003@gmail.com>
2026-03-01 15:34:35 -0300

    Primeiro commit

---
+++ Integrador de Modelos de ML/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Integrador_de_Modelos_de_ML
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
