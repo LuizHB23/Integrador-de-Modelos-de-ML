137061ef9c4c8f4e20b935db79f96dbd56d3a35d
LuizHB23 <lhb191003@gmail.com>
2026-03-05 18:29:06 -0300

    Alterando página InserirModelo.xaml

--- Integrador de Modelos de ML/Pages/InserirModelo.xaml.cs
+++ Integrador de Modelos de ML/Pages/InserirModelo.xaml.cs
@@ -21,48 +21,5 @@ namespace Integrador_de_Modelos_de_ML.Pages
             InitializeComponent();
             _gereciador = new ModeloGerenciador();
         }
-
-        private void BtnCarregarModelo_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            openFileDialog.Title = "Selecione um arquivo";
-            openFileDialog.Multiselect = false;
-
-            bool? result = openFileDialog.ShowDialog();
-
-            if (result == true)
-            {
-                caminhoModelo = openFileDialog.FileName;
-
-                string modelo = Path.GetFileName(caminhoModelo);
-                TextBoxCaminhoModelo.Text = modelo;
-            }
-        }
-
-        private void BtnProximo_Click(object sender, RoutedEventArgs e)
-        {
-            string? nome = TextBoxNome.Text;
-            string? tipo = ComboBoxTipo.Text;
-
-            // Configuração colunas não existe mais, está para ser feito nesta mesma pagina
-            // Mudando para PipelineModelo
-
-            if ((!string.IsNullOrEmpty(nome.Trim())) && (!string.IsNullOrEmpty(tipo)) && (!string.IsNullOrEmpty(caminhoModelo)))
-            {
-                var caminhoDestino = _gereciador.Salvar(caminhoModelo);
-                ModeloDTO modeloNovo = new ModeloDTO(nome, tipo, caminhoDestino);
-                InserirFrame.Navigate(new PipelineModelo());
-            }
-            else
-            {
-                MessageBox.Show($"Preencha cada parte adequadamente");
-            }
-        }
-
-        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
-        {
-            InserirFrame.Navigate(new Home());
-        }
     }
 }
