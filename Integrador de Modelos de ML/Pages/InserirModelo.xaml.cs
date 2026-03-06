20a460e2b356d746d6d6425cff4fdcf4bcd8815e
LuizHB23 <lhb191003@gmail.com>
2026-03-06 15:47:41 -0300

    Protótipo página InserirModelo concluída

--- Integrador de Modelos de ML/Pages/InserirModelo.xaml.cs
+++ Integrador de Modelos de ML/Pages/InserirModelo.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using InetradorAplicacao.DTO;
 using InetradorAplicacao.Gerenciador;
 using IntegradorDominio;
+using IntegradorDominio.WPF;
 using Microsoft.Win32;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Text.Json;
 
 namespace Integrador_de_Modelos_de_ML.Pages
 {
@@ -21,5 +23,21 @@ namespace Integrador_de_Modelos_de_ML.Pages
             InitializeComponent();
             _gereciador = new ModeloGerenciador();
         }
+
+        public void BtnCarregarColunas_Click(string caminhoArquivo)
+        {
+            using(var sr =  new StreamReader(caminhoArquivo))
+            {
+                string texto = sr.ReadToEnd();
+                var listaColunaSchema = JsonSerializer.Deserialize<List<ColunaSchema>>(texto);
+
+                // O ItemsControl vai gerar um Card para cada item desta lista automaticamente!
+                ListaColunasSchema.ItemsSource = listaColunaSchema;
+            }
+        }
+
+        private void BtnProximo_Click(object sender, RoutedEventArgs e) => NavigationService.Navigate(new PipelineModelo());
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e) => NavigationService.Navigate(new Home());
+
     }
 }
---
+++ IntegradorDominio/WPF/ColunaSchema.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegradorDominio.WPF
+{
+    public class ColunaSchema
+    {
+        public string NomeColuna { get; set; }
+        public string TipoDados { get; set; }
+        public string Finalidade { get; set; }
+        public bool IsCategorico { get; set; }
+    }
+}
