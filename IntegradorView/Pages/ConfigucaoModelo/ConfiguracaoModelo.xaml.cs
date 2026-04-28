9bb3eb8d622bad72a827fcb55be7b3b20e7e5705
LuizHB23 <lhb191003@gmail.com>
2026-04-27 21:56:41 -0300

    Ajustando a Home e criando uma nova página

---
+++ IntegradorView/Pages/ConfigucaoModelo/ConfiguracaoModelo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntegradorView.Pages.ConfigucaoModelo
+{
+    /// <summary>
+    /// Interação lógica para ConfiguracaoModelo.xam
+    /// </summary>
+    public partial class ConfiguracaoModelo : UserControl
+    {
+        public ConfiguracaoModelo()
+        {
+            InitializeComponent();
+        }
+    }
+}
